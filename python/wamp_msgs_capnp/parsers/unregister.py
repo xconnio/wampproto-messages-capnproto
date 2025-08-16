fa8579d25de9ea8cbe1f04975f53395c8b6b8a12
Mahad <mahad@xconn.io>
2025-08-16 16:38:05 +0500

    add unregister parser

---
+++ python/wamp_msgs_capnp/parsers/unregister.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+import capnp
+from wampproto.messages import unregister as unregister_message
+
+from wamp_msgs_capnp.parsers import helpers
+
+# Load the Cap'n Proto schema
+root_dir = Path(__file__).resolve().parents[1]
+schema_file = os.path.join(root_dir, "schemas", "unregister.capnp")
+unregister_capnp = capnp.load(str(schema_file))
+
+
+class Unregister(unregister_message.IUnregisterFields):
+    def __init__(self, gen):
+        self._gen = gen
+
+    @property
+    def request_id(self) -> int:
+        return self._gen.requestID
+
+    @property
+    def registration_id(self) -> int:
+        return self._gen.registrationID
+
+
+def unregister_to_capnproto(u: unregister_message.Unregister) -> bytes:
+    unregister = unregister_capnp.Unregister.new_message()
+    unregister.requestID = u.request_id
+    unregister.registrationID = u.registration_id
+
+    packed_data = unregister.to_bytes_packed()
+    return helpers.prepend_header(unregister_message.Unregister.TYPE, packed_data, b"")
+
+
+def capnproto_to_unregister(data: bytes) -> unregister_message.Unregister:
+    message_data, _ = helpers.extract_message(data)
+    unregister_obj = unregister_capnp.Unregister.from_bytes_packed(message_data)
+
+    return Unregister(unregister_obj)
