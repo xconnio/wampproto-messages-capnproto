7bc47e327b90c9604a4a866011bc1a01c93cd42f
Mahad <mahad@xconn.io>
2025-08-22 18:52:36 +0500

    fix typo in excludeMe in python

--- python/wamp_msgs_capnp/parsers/publish.py
+++ python/wamp_msgs_capnp/parsers/publish.py
@@ -30,7 +30,7 @@ class PublishFields(IPublishFields):
     def options(self) -> Optional[dict[str, Any]]:
         details = {}
 
-        if not self._gen.exludeMe:
+        if not self._gen.excludeMe:
             details["exclude_me"] = False
 
         return details
